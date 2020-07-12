376ef94be962bd032de5fb2c63b0b53cca795532
4-codedev <53953518+4-codedev@users.noreply.github.com>
2020-07-11 20:24:35 -0700

    Add files via upload
    A simple class to solve algebra problems.

---
+++ Algebra.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace AlgabraPointPloter
+{
+    public class Algebra
+    {
+        public Algebra()
+        {
+
+        }
+        public int Linear(int a, int b, int c, int x)
+        {
+            int q = a * x;
+            int d = c - q;
+            int y = d / b;
+            return y;
+        }
+
+        public int Parabola(int x)
+        {
+            int y = Math.Pow(x, 2);
+            return y;
+        }
+        public int Hyperbola(int x, int n)
+        {
+            int y = n / x;
+
+            return y;
+        }
+    }
+}
